c39ed4e45c76b2c11fd11a5cc559d905c4eb5cfc
Will Zegers <will.zegers@gmail.com>
2016-06-21 13:57:23 -0700

    Added projectile (lasers) and appropriate physics

--- Laser Defender/Assets/Scripts/EnemySpawner.cs
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class EnemySpawner : MonoBehaviour {
 	public GameObject enemyPrefab;
+	public float width = 10.92f;
+	public float height = 11.5f;
+
+	private int direction = 1;
+	public float speed = 3.0f;
+	private float xMin;
+	private float xMax;
 
 	// Use this for initialization
 	void Start () {
@@ -11,10 +18,29 @@ public class EnemySpawner : MonoBehaviour {
 			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			enemy.transform.parent = child.transform;
 		}
+
+		float cameraDistance = transform.position.z - Camera.main.transform.position.z;
+		xMin = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, cameraDistance)).x;
+		xMax = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, cameraDistance)).x;
+		Mathf.Clamp (speed, 1, 10);
+	}
+
+	private float getSpeed() {
+		return speed * Time.deltaTime;
+	}
+
+	void OnDrawGizmos () {
+		Gizmos.DrawWireCube (transform.position, new Vector3 (width, height));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (transform.position.x >= (xMax - width/2.2)) {
+			direction = -1;
+		} else if (transform.position.x <= (xMin + width/2.2)) {
+			direction = 1;
+		}
+		transform.position += new Vector3 (direction * getSpeed (), 0);
 	}
 }
--- Laser Defender/Assets/Scripts/PlayerController.cs
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Spaceship : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
 	public float speed = 15.0f;
+	public float projectileSpeed = 0f;
+	public float firingRate = 0.2f;
 	public float padding = 0.5f;
+	public GameObject projectile;
 
 	private float xMin = -5;
 	private float xMax = 5;
@@ -15,6 +18,11 @@ public class Spaceship : MonoBehaviour {
 		xMin = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, cameraDistance)).x;
 		xMax = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, cameraDistance)).x;
 	}
+
+	private void Fire() {
+		GameObject beam = Instantiate(projectile, new Vector3(this.transform.position.x, this.transform.position.y+0.5f), Quaternion.identity) as GameObject;
+		beam.rigidbody2D.velocity = new Vector3(0,projectileSpeed, 0);		
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,6 +31,12 @@ public class Spaceship : MonoBehaviour {
 			transform.position += Vector3.left * speed * Time.deltaTime;
 		} else if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < (xMax - padding)) {
 			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			InvokeRepeating("Fire",0.000001f, firingRate);
+		} else if (Input.GetKeyUp (KeyCode.Space)) {
+			CancelInvoke("Fire");
 		}
 		
 	}
