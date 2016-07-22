09583af02785470faac69c8c1a66be5ac0fa9fdc
Will Zegers <will.zegers@gmail.com>
2016-07-22 01:44:02 -0700

    Added unit testing for ActionMaster

---
+++ Bowlmaster/Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ActionMasterTest {
+
+	private ActionMaster actionMaster;
+	private ActionMaster.Action tidy    = ActionMaster.Action.Tidy;
+	private ActionMaster.Action reset   = ActionMaster.Action.Reset;
+	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+	private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
+
+	[SetUp]
+	public void Setup() {
+		actionMaster = new ActionMaster ();
+	}
+
+	[Test]
+	public void T00_PassingTest() {
+		Assert.AreEqual (1, 1);
+	}
+
+	[Test]
+	public void T01_OneStrikeReturnsEndTurn() {
+		Assert.AreEqual (endTurn, actionMaster.Bowl(10));
+	}
+
+	[Test]
+	public void T02_Bowl8ReturnsTidy() {
+		Assert.AreEqual (tidy, actionMaster.Bowl (8));
+	}
+
+	[Test]
+	public void T03_BowlEndFrameReturnsEndTurn() {
+		actionMaster.Bowl (3);
+		Assert.AreEqual (endTurn, actionMaster.Bowl (3));
+	}
+
+	[Test]
+	public void T04_Bowl2And8SpareReturnEndTurn() {
+		Assert.AreEqual (tidy, actionMaster.Bowl (2));
+		Assert.AreEqual (endTurn, actionMaster.Bowl (8));
+	}
+
+	[Test]
+	public void T05_ThirdPlayInLastFrameReturnsEndGame() {
+		for (int i = 1; i < 21; i++) {
+			actionMaster.Bowl (0);
+		}
+		Assert.AreEqual (endGame, actionMaster.Bowl (0));
+	}
+
+	[Test]
+	public void T06_NoStrikeLastFrameReturnsEndGame() {
+		for (int i = 1; i < 18; i++) {
+			actionMaster.Bowl (10);
+		}
+		actionMaster.Bowl (0);
+		Assert.AreEqual (endGame, actionMaster.Bowl (0));
+	}
+
+	[Test]
+	public void T07_SpareOnLastFrameReturnsReset() {
+		for (int i = 1; i < 19; i++) {
+			actionMaster.Bowl(0);
+		}
+		actionMaster.Bowl (2);
+		Assert.AreEqual(reset, actionMaster.Bowl(8));
+	}
+
+	[Test]
+	public void T08_StrikeOnLastFrameReturnsReset() {
+		for (int i = 0; i < 19; i++) {
+			actionMaster.Bowl (0);
+		}
+		Assert.AreEqual (reset, actionMaster.Bowl (10));
+	}
+
+	[Test]
+	public void T09_StrikeOnPlay1NoStrikeOnPlay2LastFrameReturnsReset() {
+		for (int i = 1; i < 19; i++) {
+			actionMaster.Bowl (0);
+		}
+		actionMaster.Bowl (10);
+		Assert.AreEqual (reset, actionMaster.Bowl (0));
+	}
+}
---
+++ Bowlmaster/Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionMaster {
+
+	public enum Action {
+		Tidy,
+		Reset,
+		EndTurn,
+		EndGame
+	}
+	private int bowl = 1;
+	private int[] bowls = new int[22];
+
+	public Action Bowl(int pins) {
+
+		if (pins < 0 || pins > 10) {
+			throw new UnityException ("Invalid pins!");
+		}
+
+		bowls [bowl] = pins;
+		if (bowl == 21) {
+			return Action.EndGame;
+		}
+
+		if (pins == 10) {
+			if (bowl == 19 || bowl == 20) {
+				bowl += 1;
+				return Action.Reset;
+			} else {
+				bowl += 2;
+				return Action.EndTurn;
+			}
+		}
+
+		if (bowl == 20 && pins + bowls[bowl-1] == 10) {
+			bowl += 1;
+			return Action.Reset;
+		} else if (bowl % 2 != 0) {
+			bowl += 1;
+			return Action.Tidy;
+		} else {
+			bowl += 1;
+			return Action.EndTurn;
+		}
+			
+		throw new UnityException ("Not sure what action to return!");
+	}
+}
